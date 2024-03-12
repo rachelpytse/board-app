c04bea5b2aa86a6f225bc03817ed1a419a6f0d60
rach <rachelpytse@gmail.com>
2024-03-12 13:48:53 +0100

    Added mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,32 @@
-import { Button } from "@/components/ui/button"
+"use client"
+
 import Image from "next/image"
+import { api } from "@/convex/_generated/api"
+import { useOrganization } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button"
+import { useApiMutation } from "@/hooks/use-api-mutation"
+import { toast } from "sonner"
 
 export const EmptyBoards = () => {
+    const {organization} = useOrganization()
+    //useMutation can also work too but here we use hook method
+    //using hook method instead of useMutation it will be simpler and less code but we couldn't get the types to work so we lose types
+    const {mutate, pending} = useApiMutation(api.board.create)
+
+    const onClick = () => {
+        if(!organization) return;
+        
+        mutate({
+            orgId: organization?.id,
+            title: "Untitled"
+        })
+            .then((id) => {
+                toast.success("Board created")
+                //TODO: redirect to board/{id}
+            })
+            .catch((error) => toast.error("Failed to create board"))
+    }
+
     return(
         <div className="h-full flex flex-col items-center justify-center">
             <Image
@@ -17,7 +42,7 @@ export const EmptyBoards = () => {
                 Start by creating a board for your organization.
             </p>
             <div className="mt-6">
-                <Button size="lg">
+                <Button disabled={pending} onClick={onClick} size="lg">
                     Create board
                 </Button>
             </div>
