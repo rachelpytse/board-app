87e6584e46aafd4c0703dba6253417504078fb53
rach <rachelpytse@gmail.com>
2024-03-17 17:45:24 +0100

    Added moveToFront, moveToBack and selectionNet function

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -5,7 +5,7 @@ import { useCallback, useMemo, useState } from "react"
 import { useHistory, useCanUndo, useCanRedo, useMutation, useStorage, useOthersMapped, } from "@/liveblocks.config"
 
 import { Camera, CanvasMode, CanvasState, Color, LayerType, Point, Side, XYWH } from "@/types/canvas"
-import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/lib/utils"
+import { connectionIdToColor, findIntersectingLayersWithRectangle, pointerEventToCanvasPoint, resizeBounds } from "@/lib/utils"
 
 import { Info } from "./info"
 import { Participants } from "./participants"
@@ -109,6 +109,43 @@ export const Canvas = ({
         }
     }, [])
 
+    const updateSelectionNet = useMutation((
+        {storage, setMyPresence},
+        current: Point,
+        origin: Point,
+    ) => {
+        const layers = storage.get("layers").toImmutable()
+        setCanvasState({
+            mode: CanvasMode.SelectionNet,
+            origin,
+            current,
+        })
+
+        const ids = findIntersectingLayersWithRectangle(
+            layerIds,
+            layers,
+            origin,
+            current,
+        )
+
+        setMyPresence({selection: ids})
+    }, [layerIds])
+
+    const startMultiSelection = useCallback((
+        current: Point,
+        origin: Point,
+    ) => {
+        // Just clicking on the screen doesn't mean we want to select things, so set the threshold to 5
+        if(Math.abs(current.x - origin.x) + Math.abs(current.y - origin.y) > 5) {
+            console.log("ATTEMPTING TO SELECTION NET")
+            setCanvasState({
+                mode: CanvasMode.SelectionNet,
+                origin,
+                current,
+            })
+        }
+    }, [])
+
     const resizeSelectedLayer = useMutation((
         { storage, self },
         point: Point,
@@ -155,7 +192,11 @@ export const Canvas = ({
 
         const current = pointerEventToCanvasPoint(e, camera)
 
-        if(canvasState.mode === CanvasMode.Translating) {
+        if(canvasState.mode === CanvasMode.Pressing) {
+            startMultiSelection(current, canvasState.origin)
+        } else if (canvasState.mode === CanvasMode.SelectionNet) {
+            updateSelectionNet(current, canvasState.origin)
+        } else if(canvasState.mode === CanvasMode.Translating) {
             translateSelectedLayers(current)
         } else if(canvasState.mode === CanvasMode.Resizing) {
             resizeSelectedLayer(current)
@@ -306,6 +347,15 @@ export const Canvas = ({
                     <SelectionBox
                         onResizeHandlePointerDown={onResizeHandlePointerDown}
                     />
+                    {canvasState.mode === CanvasMode.SelectionNet && canvasState.current != null && (
+                        <rect
+                            className="fill-blue-500/5 stroke-blue-500 stroke-1"
+                            x={Math.min(canvasState.origin.x, canvasState.current.x)}
+                            y={Math.min(canvasState.origin.y, canvasState.current.y)}
+                            width={Math.abs(canvasState.origin.x - canvasState.current.x)}
+                            height={Math.abs(canvasState.origin.y - canvasState.current.y)}
+                        />
+                    )}
                     <CursorsPresence/>
                 </g>
             </svg>
